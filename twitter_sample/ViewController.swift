584f798f21a6e10302b3f5cc21320c344b9261d9
NAGAMINE HIROMASA <hiromasa.nagamine@linecorp.com>
2015-08-16 20:31:28 +0900

    Initial Commit

---
+++ twitter_sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitter_sample
+//
+//  Created by NAGAMINE HIROMASA on 2015/08/16.
+//  Copyright (c) 2015年 Hiromasa Nagamine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
