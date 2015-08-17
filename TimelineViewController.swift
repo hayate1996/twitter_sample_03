1efedb4800de48ca027418badc55c9ed0d4d3a58
NAGAMINE HIROMASA <hiromasa.nagamine@linecorp.com>
2015-08-17 12:35:03 +0900

    add twitter timeline base

---
+++ TimelineViewController.swift
@@ -0,0 +1,58 @@
+//
+//  TimelineViewController.swift
+//  twitter_sample
+//
+//  Created by NAGAMINE HIROMASA on 2015/08/16.
+//  Copyright (c) 2015年 Hiromasa Nagamine. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UITableViewController, UITableViewDelegate, UITableViewDataSource {
+
+    required init!(coder aDecoder: NSCoder!) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+
+        // Configure the cell...
+
+        return cell
+    }
+
+}
--- twitter_sample/ViewController.swift
+++ twitter_sample/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    let accountsList:[Dictionary<String,String>] = [["name":"Account AAA"], ["name":"Account BBB"], ["name":"Account CCC"]]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tappedLoginButton(sender: AnyObject) {
+        var accountSelectionSheet = UIAlertController()
+        accountSelectionSheet.title = "Select Account"
+
+        let selectAction =  { (var action: UIAlertAction!) -> Void in
+            self.performSegueWithIdentifier("segueTimelineViewController", sender: nil)
+        }
+
+        for account in accountsList as [Dictionary<String, String>] {
+            let action = UIAlertAction(title: account["name"]!, style: .Default, handler: selectAction)
+            accountSelectionSheet.addAction(action)
+        }
+
+        let CanceledAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        accountSelectionSheet.addAction(CanceledAction)
+
+        self.presentViewController(accountSelectionSheet, animated: true, completion: nil)
+    }
 
 }
 
